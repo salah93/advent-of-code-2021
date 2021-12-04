c7258b4f18f6e0cc919328a18fc1a847dedeb815
Salah Ahmed <salahs.email@pm.me>
2021-12-04 15:04:34 -0500

    day four part one

---
+++ day_four.py
@@ -0,0 +1,133 @@
+import re
+
+from collections import deque, namedtuple
+from typing import List, Optional
+
+
+class BingoItem(object):
+    def __init__(self, value: int, marked: bool = False):
+        self.value = value
+        self.marked = marked
+
+
+Position = namedtuple("Position", ["row", "column"])
+
+
+class BingoBoard(object):
+    def __init__(self):
+        self.data = []  # List[List[BingoItem]]
+
+    def add_row(self, data: List[int]):
+        bingo_data = [BingoItem(d) for d in data]
+        self.data.append(bingo_data)
+
+    @property
+    def is_populated(self):
+        is_populated = len(self.data) == 5
+        for row in self.data:
+            is_populated = is_populated and len(row) == 5
+        return is_populated
+
+    def get_row(self, row: int) -> List[BingoItem]:
+        return self.data[row]
+
+    def get_column(self, column: int) -> List[BingoItem]:
+        return [d[column] for d in self.data]
+
+    def get_item(self, pos: Position) -> BingoItem:
+        return self.data[pos.row][pos.column]
+
+    def get_positions_for_value(self, value: int) -> List[Position]:
+        positions = []  # type: List[Position]
+        for row_i, row in enumerate(self.data):
+            for col_i, item in enumerate(row):
+                if not item.marked and item.value == value:
+                    positions.append(Position(row=row_i, column=col_i))
+        return positions
+
+    def mark_item_at_position(self, pos: Position) -> BingoItem:
+        item = self.get_item(pos)
+        item.marked = True
+        return item
+
+    def mark_items_with_value(self, value: int) -> List[BingoItem]:
+        positions = self.get_positions_for_value(value)
+        items = []  # type: List[BingoItem]
+        for position in positions:
+            items.append(self.mark_item_at_position(position))
+        return items
+
+    def get_unmarked_values(self) -> List[int]:
+        items = []  # type: List[int]
+        for row in self.data:
+            for item in row:
+                if not item.marked:
+                    items.append(item.value)
+        return items
+
+    def bingo(self) -> bool:
+        return self._bingo_for_rows() or self._bingo_for_columns()
+
+    def _bingo_for_rows(self) -> bool:
+        bingo = False
+        for i in range(5):
+            bingo = bingo or all([d.marked for d in self.get_row(i)])
+        return bingo
+
+    def _bingo_for_columns(self) -> bool:
+        bingo = False
+        for i in range(5):
+            bingo = bingo or all([d.marked for d in self.get_column(i)])
+        return bingo
+
+    def display(self):
+        total_display = ""
+        for row in self.data:
+            for item in row:
+                total_display += f"*{item.value}*" if item.marked else f" {item.value} "
+                total_display += " "
+            total_display += "\n"
+        return total_display
+
+
+def main():
+    boards = []  # type: List[BingoBoard]
+    numbers_draw = deque()  # type: List[int]
+    pattern = re.compile(
+        r"^(?P<first>(\d|\s)\d )(?P<second>(\d|\s)\d )(?P<third>(\d|\s)\d )(?P<fourth>(\d|\s)\d )(?P<fifth>(\d|\s)\d)$"
+    )
+    with open("bingo.txt") as f:
+        numbers_draw = deque([int(d) for d in next(f).split(",")])
+        board = BingoBoard()
+        for line in f:
+            match = pattern.match(line.rstrip())
+            if match:
+                board.add_row([int(d) for d in match.groupdict().values()])
+            else:
+                board = BingoBoard()
+            if board.is_populated:
+                boards.append(board)
+
+    winning_board = None  # type: Optional[BingoBoard]
+    winning_number = None  # type: Optional[int]
+    number_drawn = numbers_draw.popleft()
+    while winning_board is None and numbers_draw:
+        for board in boards:
+            board.mark_items_with_value(number_drawn)
+            if board.bingo():
+                winning_board = board
+                winning_number = number_drawn
+        number_drawn = numbers_draw.popleft()
+
+    if winning_board:
+        print(
+            f"""
+                winning draw = {winning_number}
+                board =
+{winning_board.display()}
+                score = {sum(winning_board.get_unmarked_values()) * winning_number}
+            """
+        )
+
+
+main()
