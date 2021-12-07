284049e1e127763d3f07158d8bfa0c8324e3da63
Salah Ahmed <salahs.email@pm.me>
2021-12-06 22:09:18 -0500

    track fish count with dictionary

--- day_six.py
+++ day_six.py
@@ -1,58 +1,40 @@
+from collections import defaultdict
+from collections.abc import MutableMapping
 from typing import List, Optional
-from functools import reduce
-
-
-class Fishy(object):
-    def __init__(self, initial_days_remaining: int):
-        self.days_remaining = initial_days_remaining
-
-    def decrement(self):
-        # type: () -> Optional[Fishy]
-        new_days_remaining = self.days_remaining - 1
-        new_fishy = None
-        if new_days_remaining < 0:
-            new_days_remaining = 6
-            new_fishy = Fishy(8)
-        self.days_remaining = new_days_remaining
-        return new_fishy
-
-    def get_children(self):
-        # type: () -> List[Fishy]
-        fishies = self.children
-        for fishy in self.children:
-            fishies.extend(fishy.get_children())
-        return fishies
 
 
 class Pond(object):
-    def __init__(self, fishies: List[Fishy]):
-        self.fishies = fishies
+    def __init__(self, fishies: List[int]):
+        self.fishies = defaultdict(int)
+        for days_remaining in fishies:
+            self.fishies[days_remaining] += 1
 
     def new_day(self):
-        new_fishies = []
-        for fishy in self.fishies:
-            new_fishy = fishy.decrement()
-            if new_fishy:
-                new_fishies.append(new_fishy)
-        self.fishies.extend(new_fishies)
+        new_fishies = defaultdict(int)
+        for days_remaining in sorted(self.fishies, reverse=True):
+            new_days_remaining = days_remaining - 1
+            if new_days_remaining < 0:
+                new_days_remaining = 6
+                new_fishies[8] += self.fishies[days_remaining]
+            new_fishies[new_days_remaining] += self.fishies[days_remaining]
+        self.fishies = new_fishies
+
+    def get_count(self) -> int:
+        return sum([v for v in self.fishies.values()])
 
 
 def main():
     with open("data/lanternfish.txt") as f:
         lantern_fish = [
-            Fishy(int(fishy_days_remaining))
-            for fishy_days_remaining in next(f).split(",")
-        ]  # type: List[Fishy]
+            int(fishy_days_remaining) for fishy_days_remaining in next(f).split(",")
+        ]  # type: List[int]
     pond = Pond(lantern_fish)
 
-    iter_days = 80
-
-    def get_fishy_count(a: int, b: Fishy) -> int:
-        return a + 1 + len(b.get_children())
+    iter_days = 256
 
     for day_i in range(1, iter_days + 1):
         pond.new_day()
-        print(f"fishy count after {day_i} days is {len(pond.fishies)}")
+        print(f"fishy count after {day_i} days is {pond.get_count()}")
 
 
 main()
