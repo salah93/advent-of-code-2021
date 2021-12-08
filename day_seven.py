7b605b5cf8b97f8244f1f3b8e5030d234312b60e
Salah Ahmed <salahs.email@pm.me>
2021-12-07 20:51:40 -0500

    add for part two

--- day_seven.py
+++ day_seven.py
@@ -1,19 +1,31 @@
 from typing import List
 
 
-def sum_of_costs(desired_position: int, crab_positions: List[int]):
+def sum_of_costs_part_1(desired_position: int, crab_positions: List[int]) -> int:
     return sum([abs(pos - desired_position) for pos in crab_positions])
+
+
+def sum_of_costs_part_2(desired_position: int, crab_positions: List[int]) -> int:
+    costs = []
+    for pos in crab_positions:
+        difference = abs(pos - desired_position)
+        costs.append((difference * (difference + 1)) / 2)
+    return sum(costs)
 
 
 def main():
     with open("data/crabs.txt") as f:
         crab_positions = [int(c) for c in next(f).split(",")]  # type: List[int]
 
-    min_position = (crab_positions[0], sum_of_costs(crab_positions[0], crab_positions))
-    for crab_position in crab_positions:
-        cost = sum_of_costs(crab_position, crab_positions)
+    sum_fn = sum_of_costs_part_2
+    min_position = (
+        0,
+        sum_fn(0, crab_positions),
+    )
+    for i in range(max(crab_positions)):
+        cost = sum_fn(i, crab_positions)
         if cost < min_position[1]:
-            min_position = (crab_position, cost)
+            min_position = (i, cost)
     print(f"minimum cost = {min_position[1]}, at position {min_position[0]}")
 
 
