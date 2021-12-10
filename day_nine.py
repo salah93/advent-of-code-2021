9119e96de47d719ba69f687970c3039d3ea9c01b
Salah Ahmed <salahs.email@pm.me>
2021-12-09 22:56:39 -0500

    answer for part 2

--- day_nine.py
+++ day_nine.py
@@ -1,11 +1,44 @@
-from typing import List, NamedTuple
+from typing import List, NamedTuple, Set
 from collections.abc import Iterator
+from functools import reduce
 
 
 class Position(NamedTuple):
     i: int
     j: int
     height: int
+
+    def __eq__(self, other):
+        return (self.i, self.j) == (self.i, self.j)
+
+    def __hash__(self):
+        return hash((self.i, self.j))
+
+
+class Marked(object):
+    def __init__(self, marked: bool, height: int):
+        self.marked = marked
+        self.height = height
+
+    def __eq__(self, other):
+        if isinstance(other, Marked):
+            eq = self.height == other.height
+        else:
+            eq = self.height == other
+        return eq
+
+    def __repr__(self):
+        return f"Marked<{self.height}>{'*' if self.marked else ''}"
+
+    def __lt__(self, other):
+        if isinstance(other, Marked):
+            lt = self.height < other.height
+        else:
+            lt = self.height < other
+        return lt
+
+    def __hash__(self):
+        return hash(self.height)
 
 
 class Cave(Iterator):
@@ -56,6 +89,59 @@ class Cave(Iterator):
         self._curr = (self._curr[0], self._curr[1] + 1)
         return pos
 
+    def get_basins(self) -> List[List[Position]]:
+        # if # is not 9 and pos is not marked, add number
+        # try right, try left, try up, try down
+        grid = []
+        for row_i, row in enumerate(self._grid):
+            grid.append([Marked(marked=False, height=pos) for pos in row])
+
+        def pos_is_valid(pos: Marked) -> bool:
+            return not pos.marked and pos.height != 9
+
+        def inner(row_i: int, col_j: int) -> List[Position]:
+            """process row until end or reach a 9"""
+            if row_i >= len(grid) or col_j >= len(grid) or row_i < 0 or col_j < 0:
+                return set()
+            else:
+                grid[row_i][col_j].marked = True
+                positions = [
+                    Position(i=row_i, j=col_j, height=grid[row_i][col_j].height)
+                ]
+
+                can_go_up = (row_i - 1) >= 0
+                if can_go_up:
+                    if pos_is_valid(grid[row_i - 1][col_j]):
+                        positions += inner(row_i - 1, col_j)
+
+                can_go_down = (row_i + 1) < len(grid)
+                if can_go_down:
+                    if pos_is_valid(grid[row_i + 1][col_j]):
+                        positions += inner(row_i + 1, col_j)
+
+                can_go_right = (col_j + 1) < len(grid)
+                if can_go_right:
+                    if pos_is_valid(grid[row_i][col_j + 1]):
+                        positions += inner(row_i, col_j + 1)
+
+                can_go_left = (col_j - 1) >= 0
+                if can_go_left:
+                    if pos_is_valid(grid[row_i][col_j - 1]):
+                        positions += inner(row_i, col_j - 1)
+
+                return positions
+
+        basins = []  # type: List[List[int]]
+        for row_i in range(len(grid)):
+            for col_j in range(len(grid)):
+                pos = grid[row_i][col_j]
+                if pos.marked or pos.height == 9:
+                    continue
+                else:
+                    basin = list(inner(row_i, col_j))
+                    basins.append(basin)
+        return basins
+
 
 def main():
     cave = Cave()
@@ -68,6 +154,11 @@ def main():
         if cave.pos_is_low_point(pos):
             low_points.append(pos.height)
     print(f"sum of risk levels = {len(low_points) + sum(low_points)}")
+    basins = cave.get_basins()
+    sum_of_basins = sorted([len(basin) for basin in basins], reverse=True)
+    print(
+            f"product of basins = {reduce(lambda a, b: a * b, sum_of_basins[:3], 1)}"
+    )
 
 
 main()
