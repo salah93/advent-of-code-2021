a2f495fe72aaae26daf8018008bcd217af911676
Salah Ahmed <salahs.email@pm.me>
2021-12-10 22:24:21 -0500

    add for part two

--- day_ten.py
+++ day_ten.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from typing import Dict, List
 
 
@@ -13,31 +14,59 @@ def is_opening_character(character: str) -> bool:
     return character in ["{", "[", "<", "("]
 
 
-def characters_match(stack: List[str], ending_character: str) -> bool:
+def get_matching_character(opening_character: str) -> str:
+    ending_character = ""
+    if opening_character == "{":
+        ending_character = "}"
+    elif opening_character == "<":
+        ending_character = ">"
+    elif opening_character == "(":
+        ending_character = ")"
+    elif opening_character == "[":
+        ending_character = "]"
+    else:
+        raise IllegalCharacter
+    return ending_character
+
+
+def characters_match(opening_character: str, ending_character: str) -> bool:
     try:
-        opening_character = stack.pop()
-    except IndexError:
-        opening_character == "x"
-    return (
-        (opening_character == "{" and ending_character == "}")
-        or (opening_character == "(" and ending_character == ")")
-        or (opening_character == "<" and ending_character == ">")
-        or (opening_character == "[" and ending_character == "]")
-    )
+        expected_ending_character = get_matching_character(opening_character)
+        matches = expected_ending_character == ending_character
+    except IllegalCharacter:
+        matches = False
+    return matches
 
 
-def convert_characters_to_points(illegal_characters: str) -> List[int]:
+def convert_characters_to_points_corrupt_line(
+    illegal_characters: List[str],
+) -> int:
     illegal_character_score_map = {
         ")": 3,
         "]": 57,
         "}": 1197,
         ">": 25137,
     }  # type: Dict[str, int]
-    return [illegal_character_score_map[char] for char in illegal_characters]
+    return sum([illegal_character_score_map[char] for char in illegal_characters])
+
+
+def convert_characters_to_points_incomplete_line(missing_characters: List[str]) -> int:
+    illegal_character_score_map = {
+        ")": 1,
+        "]": 2,
+        "}": 3,
+        ">": 4,
+    }  # type: Dict[str, int]
+    return reduce(
+        lambda total, char: (total * 5) + illegal_character_score_map[char],
+        missing_characters,
+        0,
+    )
 
 
 def main():
     illegal_characters = []  # type: List[str]
+    incomplete_lines = []  # type: List[List[str]]
     with open("data/syntax.txt") as f:
         for line in f:
             stack = []
@@ -45,13 +74,34 @@ def main():
                 if is_opening_character(character):
                     stack.append(character)
                 elif is_closing_character(character):
-                    if not characters_match(stack, character):
+                    try:
+                        opening_character = stack.pop()
+                    except IndexError:
+                        opening_character = "x"
+                    if not characters_match(opening_character, character):
                         illegal_characters.append(character)
                         break
                 else:
                     raise IllegalCharacter
-    points = convert_characters_to_points(illegal_characters)
-    print(f"total syntax error score is {sum(points)}")
+            else:
+                incomplete_line = []  # type: List[str]
+                while stack:
+                    opening_character = stack.pop()
+                    ending_character = get_matching_character(opening_character)
+                    incomplete_line.append(ending_character)
+                incomplete_lines.append(incomplete_line)
+    points = convert_characters_to_points_corrupt_line(illegal_characters)
+    print(f"total syntax error score is {points}")
+
+    scores_incomplete_lines = sorted(
+        [
+            convert_characters_to_points_incomplete_line(incomplete_line)
+            for incomplete_line in incomplete_lines
+        ]
+    )  # type List[int]
+    print(
+        f"middle score = {scores_incomplete_lines[int(len(scores_incomplete_lines) / 2)]}"
+    )
 
 
 main()
