780b38964315d2a905d90fbc970f5c7e69684282
Salah Ahmed <salahs.email@pm.me>
2021-12-12 16:41:05 -0500

    add day twelve part one

---
+++ day_twelve.py
@@ -0,0 +1,74 @@
+from collections import defaultdict
+from typing import Dict, List, NamedTuple, Set, Tuple
+
+
+class CaveNotFound(Exception):
+    pass
+
+
+class Cave(NamedTuple):
+    name: str
+    is_small: bool
+
+
+START = Cave("start", is_small=True)
+END = Cave("end", is_small=True)
+
+
+class Graph(object):
+    def __init__(self):
+        self._caves = set()  # type: Set[Cave]
+        self._edges = defaultdict(set)  # type: Dict[Cave, Set[Cave]]
+
+    def add_cave(self, cave: Cave):
+        self._caves.add(cave)
+
+    def add_caves_from(self, caves: List[str]):
+        for cave in caves:
+            self.add_cave(cave)
+
+    def add_edge(self, cave_a: Cave, cave_b: Cave):
+        if cave_a not in self._caves or cave_b not in self._caves:
+            raise CaveNotFound
+        self._edges[cave_a].add(cave_b)
+        self._edges[cave_b].add(cave_a)
+
+    def get_paths(self) -> List[List[Cave]]:
+        return self.explore_cave(
+            cave=START,
+            path=[],
+            all_paths=[],
+        )
+
+    def explore_cave(
+        self,
+        cave: Cave,
+        path: List[Cave],
+        all_paths: List[List[Cave]],
+    ) -> List[List[Cave]]:
+        if cave.is_small and cave in path:
+            return all_paths
+        path = path + [cave]
+        if cave == END:
+            return all_paths + [path]
+        else:
+            for next_cave in self._edges[cave]:
+                all_paths = self.explore_cave(next_cave, path, all_paths)
+            return all_paths
+
+
+def main():
+    g = Graph()
+    with open("data/network_test.txt") as f:
+        for line in f:
+            caves = [
+                Cave(name=name, is_small=name.islower())
+                for name in sorted(line.strip().split("-"))
+            ]  # type: List[Cave]
+            g.add_caves_from(caves)
+            g.add_edge(caves[0], caves[1])
+    paths = g.get_paths()
+    print(f"total paths = {len(paths)}")
+
+
+main()
